7386e020341ce25f75170ca400ade18d6f66a9f6
Firewolfs <fire95.wow@gmail.com>
2020-04-07 11:32:02 +0200

    Add updateInfo and updatePassword System

--- GSBCR.DAL/VisiteurDAO.cs
+++ GSBCR.DAL/VisiteurDAO.cs
@@ -31,6 +31,44 @@ namespace GSBCR.DAL
             }
             return vis;
         }
+
+        /// <summary>
+        /// Permet de mettre à jour les informations d'un visiteur grâce à son matricule
+        /// </summary>
+        /// <param name="matricule">Matricule du visiteur</param>
+        /// <param name="addr">Adresse du visiteur</param>
+        /// <param name="cp">Code Postal du visiteur</param>
+        /// <param name="town">Ville du visiteur</param>
+        /// <param name="phone">Numéro de téléphone du visiteur</param>
+        /// <param name="mail">Adresse mail du visiteur</param>
+        public void updateVisiteur(String matricule, String addr, String cp, String town, String phone, String mail) {
+            using (var context = new GSB_visite_LEGUILLIEREntities()) {
+                var v = context.VISITEUR.First(obj => obj.VIS_MATRICULE == matricule);
+
+                v.VIS_ADRESSE = addr;
+                v.VIS_CP = cp;
+                v.VIS_VILLE = town;
+                v.tel = phone;
+                v.mail = mail;
+
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Permet de mettre à jour le mot de passe d'un visiteur grâce à son matricule
+        /// </summary>
+        /// <param name="matricule">Matricule du visiteur</param>
+        /// <param name="password">Nouveau mot de passe du vistieur</param>
+        public void updatePassword(String matricule, String password) {
+            using (var context = new GSB_visite_LEGUILLIEREntities()) {
+                var v = context.VISITEUR.First(obj => obj.VIS_MATRICULE == matricule);
+
+                v.vis_mdp = password;
+
+                context.SaveChanges();
+            }
+        }
               
     }
 }
